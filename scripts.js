8e8518649e2bb34838156898436f17bd88c48ad1
jorozco94 <91165810+jorozco94@users.noreply.github.com>
2023-05-20 12:39:03 -0700

    chore: Async/Await version of code added

--- scripts.js
+++ scripts.js
@@ -7,15 +7,43 @@ buttonEl.addEventListener('click', dataGrab);
 tableDiv.classList.add('tableHolder');
 
 // Using .then methodology
-function dataGrab() {
+// function dataGrab() {
+//   if (!tableAdded) {
+//     buttonEl.classList.add('clicked');
+//     fetch('https://jsonplaceholder.typicode.com/users', {}).then((response) => {
+//       if (!response.ok) {
+//         throw new Error('Network response was not ok')
+//       }
+//       return response.json();
+//     }).then((data) => {
+//       data.forEach((el) => {
+//         const dataCell = document.createElement('div');
+//         const nameEl = document.createElement('p');
+//         const emailEl = document.createElement('p');
+//         dataCell.classList.add('dataCell')
+//         nameEl.textContent = `Name: ${el.name}`;
+//         emailEl.textContent = `E-mail: ${el.email}`;
+//         dataCell.append(nameEl, emailEl);
+//         tableDiv.append(dataCell);
+//       })
+//     }).catch((err) => {
+//       console.error(`Error: ${err}`)
+//     });
+//     mainEl.append(tableDiv);
+//     tableAdded = true;
+//   }
+// };
+
+// Asyn/Await methodology
+async function dataGrab() {
   if (!tableAdded) {
     buttonEl.classList.add('clicked');
-    fetch('https://jsonplaceholder.typicode.com/users', {}).then((response) => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users', {})
       if (!response.ok) {
         throw new Error('Network response was not ok')
       }
-      return response.json();
-    }).then((data) => {
+      const data = await response.json();
       data.forEach((el) => {
         const dataCell = document.createElement('div');
         const nameEl = document.createElement('p');
@@ -26,12 +54,10 @@ function dataGrab() {
         dataCell.append(nameEl, emailEl);
         tableDiv.append(dataCell);
       })
-    }).catch((err) => {
-      console.error(`Error: ${err}`)
-    });
-    mainEl.append(tableDiv);
-    tableAdded = true;
+      mainEl.append(tableDiv);
+      tableAdded = true;
+    } catch (error) {
+      console.error(`Error: ${error}`)
+    }
   }
 };
-
-// Asyn/Await methodology
