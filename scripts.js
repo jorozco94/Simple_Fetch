61bdad994e6d851faeb650ed7517380a586859e9
jorozco94 <91165810+jorozco94@users.noreply.github.com>
2023-05-19 18:06:28 -0700

    feat: Added fetching functionality to button click

--- scripts.js
+++ scripts.js
@@ -1,9 +1,15 @@
-// Fetch the names and emails from the URL
-// create elements that will hold a name and e-amil
+// assign click function to our button that will perform fetch action
+// perform fetch 
+// create elements that will hold a name and e-mail
 // loop thru each item in the JSON object and get it into the elements
 // append those elements to the dom
-fetch('https://jsonplaceholder.typicode.com/users', {}).then((response) => {
-  return response.json()
-}).then((data) => {
-  console.log(data)
-})
+const buttonEl = document.querySelector('.getInfo');
+buttonEl.addEventListener('click', dataGrab)
+
+function dataGrab() {
+  fetch('https://jsonplaceholder.typicode.com/users', {}).then((response) => {
+    return response.json()
+  }).then((data) => {
+    console.log(data)
+  })
+}
