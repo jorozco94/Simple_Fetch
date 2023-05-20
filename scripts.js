72419496c414b93f74662161addd42b4b5eaae1a
jorozco94 <91165810+jorozco94@users.noreply.github.com>
2023-05-19 18:24:17 -0700

    feat: Fetch function succesfully grabs/displays data on DOM

--- scripts.js
+++ scripts.js
@@ -4,12 +4,24 @@
 // loop thru each item in the JSON object and get it into the elements
 // append those elements to the dom
 const buttonEl = document.querySelector('.getInfo');
-buttonEl.addEventListener('click', dataGrab)
+const tableDiv = document.createElement('div');
+const mainEl = document.querySelector('main');
+
+buttonEl.addEventListener('click', dataGrab);
+tableDiv.classList.add('tableHolder');
 
 function dataGrab() {
   fetch('https://jsonplaceholder.typicode.com/users', {}).then((response) => {
     return response.json()
   }).then((data) => {
     console.log(data)
-  })
+    data.forEach((el) => {
+      const nameEl = document.createElement('p');
+      const emailEl = document.createElement('p');
+      nameEl.textContent = `Name: ${el.name}`;
+      emailEl.textContent = `E-mail: ${el.email}`;
+      tableDiv.append(nameEl, emailEl);
+    })
+  });
+  mainEl.append(tableDiv);
 }
