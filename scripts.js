c12ee6066821c53eedf88fad8649573a74761bc1
jorozco94 <91165810+jorozco94@users.noreply.github.com>
2023-05-19 18:33:26 -0700

    chore: Stopped repeated adding of table to DOM

--- scripts.js
+++ scripts.js
@@ -6,22 +6,27 @@
 const buttonEl = document.querySelector('.getInfo');
 const tableDiv = document.createElement('div');
 const mainEl = document.querySelector('main');
+let tableAdded = false;
 
 buttonEl.addEventListener('click', dataGrab);
 tableDiv.classList.add('tableHolder');
 
 function dataGrab() {
-  fetch('https://jsonplaceholder.typicode.com/users', {}).then((response) => {
-    return response.json()
-  }).then((data) => {
-    console.log(data)
-    data.forEach((el) => {
-      const nameEl = document.createElement('p');
-      const emailEl = document.createElement('p');
-      nameEl.textContent = `Name: ${el.name}`;
-      emailEl.textContent = `E-mail: ${el.email}`;
-      tableDiv.append(nameEl, emailEl);
-    })
-  });
-  mainEl.append(tableDiv);
-}
+  if (!tableAdded) {
+    fetch('https://jsonplaceholder.typicode.com/users', {}).then((response) => {
+      return response.json();
+    }).then((data) => {
+      data.forEach((el) => {
+        const dataCell = document.createElement('div');
+        const nameEl = document.createElement('p');
+        const emailEl = document.createElement('p');
+        nameEl.textContent = `Name: ${el.name}`;
+        emailEl.textContent = `E-mail: ${el.email}`;
+        dataCell.append(nameEl, emailEl);
+        tableDiv.append(dataCell);
+      })
+    });
+    mainEl.append(tableDiv);
+    tableAdded = true;
+  }
+};
